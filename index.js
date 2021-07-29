2de906096fef867ccdfc063e2a5dd929205c4efb
agenis <artyomgenis@gmail.com>
2021-07-29 22:50:15 +0300

    minor changes #3

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require("cors");
+require("dotenv").config();
 
 app.use(express.json());
 app.use(cors());
@@ -11,7 +12,7 @@ const usersRouter = require('./routes/Users');
 app.use("/users", usersRouter);
 
 db.sequelize.sync().then(() => {
-    app.listen(3001, () => {
-        console.log("Server is OK (port 3001)");
+    app.listen(process.env.PORT || 3001, () => {
+        console.log("Server is OK!");
     });
 });
